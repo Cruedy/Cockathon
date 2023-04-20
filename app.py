f53b62ce183878b2b898f71b8ed0314e39fe0f11
Joooshe <jgarciakimble@gmail.com>
2023-04-20 09:45:42 -0700

    Added some style to drink page

--- app.py
+++ app.py
@@ -25,12 +25,12 @@ descriptionsL = [0]
 def show_drinks():
   print("data:", dataL[0])
   print("drinks:", drinksL[0])
-  return render_template("drink.html", drinks = drinksL[0], description = descriptionsL[0],cache_timeout=0)
+  return render_template("drink.html", drinks = drinksL[0], description = descriptionsL[0], data=dataL[0], cache_timeout=0)
 
 @app.route('/retrieve_drinks', methods=['POST'])
 def retrieve_drinks():
     data = request.get_json()
-    dataL[0] = data
+    dataL[0] = ", ".join(data)
     # print("Data:", data)
     drink = drinks_to_ingredients.getDrinks(data)[0]
     drinksL[0] = drink
