51f17ae68b9792844f69fa0711323324289b81be
Joooshe <jgarciakimble@gmail.com>
2023-04-19 23:33:04 -0700

    Server not rendering the drink.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import csv
 
 app = Flask("Cockathon")
@@ -13,8 +13,13 @@ def getDrinks(tags):
         #print(row)
         if t in row[1]:
           per_tag.append(row[0])
+    file.close()
     drinks.append(per_tag)
   return drinks
+
+@app.route('/show_drinks', methods=['GET'])
+def show_drinks():
+  return render_template("drink.html", cache_timeout=0)
 
 @app.route('/retrieve_drinks', methods=['POST'])
 def retrieve_drinks():
@@ -22,8 +27,9 @@ def retrieve_drinks():
     print("Data:", data)
     drinks = getDrinks(data)
     print(drinks)
+    print("test")
     print()
-    return render_template("website.html")
+    return redirect(url_for('show_drinks'))
 
 
 @app.route('/')
